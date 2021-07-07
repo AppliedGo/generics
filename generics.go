6a37122e221d0b5089d043ccd6dabe7454394e58
christophberger <christophberger@users.noreply.github.com>
2021-07-07 18:42:16 +0200

    feat: add update #3. fix: stray package & import

--- generics.go
+++ generics.go
@@ -1,4 +1,7 @@
-package main //go:generate genny -in=capsule/capsule.go -out=capsule/uint32capsule.go gen "Item=uint32"
+//go:generate genny -in=capsule/capsule.go -out=capsule/uint32capsule.go gen "Item=uint32"
+
+package main
+
 import (
 	"fmt"
 	"reflect"
@@ -43,6 +46,8 @@ This article examines the nature of generics, and surveys various techniques tha
 **UPDATE: The "..." in the title is meant to be a plain English ellipsis!** Big thanks to the readers who have pointed out that "..." in this context can (and will) be understood as Go's ellipsis. In this case the title would of course be complete rubbish.
 
 **UPDATE 2:** The snappy title is often misunderstood as "Go needs no generics". This is far from what the article wants to say. I do see the usefulness of generics in certain problem domains, and I am the last one to balk at the idea of generics in Go. If anyone feels misled by the title, I apologize.
+
+**UPDATE 3:** Go generics are on their way, so this article will soon be of historical interest only. While generics are still a draft, I took my [balanced tree code](https://appliedgo.net/balancedtree) and [turned it into a generic tree](https://appliedgo.net/generictree), to see how easy it is to convert container types into generic ones.
 - - -
 
 ## First, an important note
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.13
+
+require github.com/cheekybits/genny v1.0.0
